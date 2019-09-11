b66df23fee05e32e77e4570f0009874821477e84
ansi90 <51782604+ansi90@users.noreply.github.com>
2019-09-11 14:05:36 +0100

    Update file.R

--- OncoRegimenFinderV3/R/file.R
+++ OncoRegimenFinderV3/R/file.R
@@ -3,25 +3,26 @@ create_regimens <- function(connectionDetails, cdmDatabaseSchema, writeDatabaseS
   connection <-  DatabaseConnector::connect(connectionDetails)
   
   sql <- SqlRender::render(SqlRender::readSql("SQL/CohortBuild.sql"), cdmDatabaseSchema = cdmDatabaseSchema, writeDatabaseSchema = writeDatabaseSchema, cohortTable = cohortTable, regimenTable = regimenTable, drug_classification_id_input = drug_classification_id_input) 
-  #sql <- SqlRender::translate(sql,targetDialect = connectionDetails$dbms)
+  sql <- SqlRender::translate(sql,targetDialect = connectionDetails$dbms)
   
   DatabaseConnector::executeSql(connection, sql)
   
-  sql <- SqlRender::render(SqlRender::readSql("SQL/RegimenCalc2.sql"), writeDatabaseSchema = writeDatabaseSchema, cohortTable = cohortTable, regimenTable = regimenTable, date_lag_input = date_lag_input) 
- # sql <- SqlRender::translate(sql,targetDialect = connectionDetails$dbms)
+  sql <- SqlRender::render(SqlRender::readSql("SQL/RegimenCalc2.sql"), writeDatabaseSchema = writeDatabaseSchema, regimenTable = regimenTable, date_lag_input = date_lag_input) 
+  sql <- SqlRender::translate(sql,targetDialect = connectionDetails$dbms)
   
+ 
   for(i in c(1:regimen_repeats)){DatabaseConnector::executeSql(connection, sql)}
   
   if(generateVocabTable){
     
-    sql <- SqlRender::render(SqlRender::readSql("SQL/RegimenVoc.sql"), writeDatabaseSchema = writeDatabaseSchema, vocabularyTable = vocabularyTable)
+    sql <- SqlRender::render(SqlRender::readSql("SQL/RegimenVoc.sql"), writeDatabaseSchema = writeDatabaseSchema, cdmDatabaseSchema = cdmDatabaseSchema, vocabularyTable = vocabularyTable)
     sql <- SqlRender::translate(sql,targetDialect = connectionDetails$dbms)
     DatabaseConnector::executeSql(connection, sql)
     
     }
   
   sql <- SqlRender::render(SqlRender::readSql("SQL/RegimenFormat.sql"), writeDatabaseSchema = writeDatabaseSchema, cohortTable = cohortTable, regimenTable = regimenTable, regimenIngredientTable = regimenIngredientTable, vocabularyTable = vocabularyTable) 
-  #sql <- SqlRender::translate(sql,targetDialect = connectionDetails$dbms)
+  sql <- SqlRender::translate(sql,targetDialect = connectionDetails$dbms)
   
   DatabaseConnector::executeSql(connection, sql)
   
