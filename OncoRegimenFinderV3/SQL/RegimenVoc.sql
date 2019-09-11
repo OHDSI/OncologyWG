f038d30a75b51cc8f2ee76d094c39c51312acb22
ansi90 <51782604+ansi90@users.noreply.github.com>
2019-09-11 14:01:31 +0100

    Update RegimenVoc.sql

--- OncoRegimenFinderV3/SQL/RegimenVoc.sql
+++ OncoRegimenFinderV3/SQL/RegimenVoc.sql
@@ -2,7 +2,8 @@ with CTE as (
 select c1.concept_name as reg_name, 
 		 listagg(lower(c2.concept_name), ',') within group (order by lower(c2.concept_name) asc) as combo_name, 
 		 c1.concept_id
-from @cdmDatabaseSchema.concept_relationship join concept c1 on c1.concept_id=concept_id_1 
+from @cdmDatabaseSchema.concept_relationship 
+join @cdmDatabaseSchema.concept c1 on c1.concept_id=concept_id_1 
 join @cdmDatabaseSchema.concept c2 on c2.concept_id=concept_id_2
 		where c1.vocabulary_id='HemOnc' and relationship_id='Has antineoplastic'
 group by c1.concept_name,c1.concept_id
