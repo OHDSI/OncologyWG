b242b018f6ea7fd24c088a6f790fdd93e4a686d4
Meera Y. Patel, MD <54548748+patelm9@users.noreply.github.com>
2019-09-12 10:18:44 -0400

    Update test_plot.R

--- r_survival/test_plot.R
+++ r_survival/test_plot.R
@@ -1,11 +1,11 @@
-source('G:/GitHub/OncologyWG/r_survival/plot_functions.R')
+source('plot_functions.R')
 
-TEST_DATA <- readRDS("./r_survival/test_data.RData")
-plot_survival(native_dataframe = DATA_00,
+TEST_DATA <- readRDS("test_data.RData")
+plot_survival(native_dataframe = TEST_DATA,
               survival_time_col = survival_time_months,
               event_col = event_occurred,
               cohort_col = cohort_definition)
 
-plot_time_to_rx_hist(native_dataframe = DATA_00, 
+plot_time_to_rx_hist(native_dataframe = TEST_DATA, 
                      target_value_col = dx_to_rx_time_days, 
                      cohort_col = cohort_definition)
