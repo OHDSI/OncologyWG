33ee0da270a89b8857e8ac0b73e713840a4dc4a4
Meera Y. Patel, MD <54548748+patelm9@users.noreply.github.com>
2019-09-12 10:21:05 -0400

    Delete 02_plot_simulated_data.R

--- r_survival/simulated/02_plot_simulated_data.R
+++
@@ -1,9 +0,0 @@
-source('G:/GitHub/OncologyWG/r_survival/simulated/plot_functions.R')
-
-plot_survival(native_dataframe = DATA_00,
-              survival_time_col = survival_time_months,
-              event_col = event_occurred)
-
-plot_time_to_rx_hist(native_dataframe = DATA_00, 
-                     target_value_col = dx_to_rx_time_days, 
-                     cohort_col = cohort_definition)
