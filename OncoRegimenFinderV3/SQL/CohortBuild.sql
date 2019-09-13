346b679cbb84a7a3dece81b7bdb6749746fc5d92
A Siapos <51782604+ansi90@users.noreply.github.com>
2019-09-13 15:43:47 +0100

    Update CohortBuild.sql

--- OncoRegimenFinderV3/SQL/CohortBuild.sql
+++ OncoRegimenFinderV3/SQL/CohortBuild.sql
@@ -14,7 +14,26 @@ inner join @cdmDatabaseSchema.concept_ancestor ca on ca.descendant_concept_id = 
 inner join @cdmDatabaseSchema.concept c on c.concept_id = ca.ancestor_concept_id
     where c.concept_id in (
           select descendant_concept_id as drug_concept_id from @cdmDatabaseSchema.concept_ancestor ca1
-          where ancestor_concept_id in (@drug_classification_id_input) /* Drug concept_id  */ 
+          where ancestor_concept_id in (
+										SELECT distinct c1.concept_id
+										FROM  @cdmDatabaseSchema.concept c1 JOIN @cdmDatabaseSchema.concept_ancestor ca1 ON ca1.descendant_concept_id = c1.concept_id
+																			JOIN @cdmDatabaseSchema.concept c2           ON ca1.ancestor_concept_id = c2.concept_id                         
+										WHERE c1.concept_class_id = 'Ingredient'
+										AND ca1.ancestor_concept_id IN(
+											  35807188  --Chemotherapeutic
+											, 35807205  --Endocrine therapeutic
+											, 35807267  --Enzyme
+											, 35807277  --Hypomethylating agent
+											, 35807493  --Immunosuppresant
+											, 35807335  --Immunosuppressant
+											, 35807189  --Immunotherapeutic
+										)
+										AND EXISTS(
+										  SELECT 1
+										  FROM @cdmDatabaseSchema.concept_relationship cr1
+										  WHERE c1.concept_id = cr1.concept_id_1 AND cr1.relationship_id IN('Rx antineopl of', 'Rx immunosuppr of')
+										)
+		  ) /* Drug concept_id  */ 
 )
 and c.concept_class_id = 'Ingredient'
 )
@@ -22,5 +41,5 @@ select *
 into @writeDatabaseSchema.@cohortTable
 from CTE_second;
 
-select * into  @writeDatabaseSchema.@regimenTable 
+select * into  @writeDatabaseSchema.@regimenTable
 from @writeDatabaseSchema.@cohortTable;
