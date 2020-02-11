eef91d9b8a336229edc4169620ba38d836fbf3ed
Michael Gurley <m-gurley@northwestern.edu>
2020-02-11 10:25:14 -0500

    referenes #241 Remove hardcoded schema name in DDL.

--- etl/cdm_source_provenance.sql
+++ etl/cdm_source_provenance.sql
@@ -1,6 +1,6 @@
 --This is DDL for the NAACCR ETL SQL provenance format.
 
-CREATE TABLE omop.cdm_source_provenance(
+CREATE TABLE cdm_source_provenance(
   cdm_event_id           BIGINT NOT NULL,
   cdm_field_concept_id   INTEGER NOT NULL,
   record_id              varchar(255) NOT NULL
