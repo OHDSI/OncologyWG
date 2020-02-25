4b775e1b2c91948af62ce778f84e074cae5905ff
Meera Patel <patelm9@mskcc.org>
2020-02-25 10:41:43 -0500

    update oncoPlot R documentation

--- oncoPlot/R/plot_survival.R
+++ oncoPlot/R/plot_survival.R
@@ -1,16 +1,15 @@
-#' Function to plot survival curve
-#' @param dbms DBMS runnong on the OMOP server
-#' @param survival_time_col numberic variable that indicates survival time
-#' @param event_col boolean variable that indicates event occurrence
-#' @param cohort_cols variable names of length 1 or greater that indicates grouping variables for output
-#' @param pval TRUE if pval is to be included in the plot
-#' @param median_survival_time TRUE if median survival time should be returned
+#' Plot a Kaplan-Meier Curve
+#' @param dbms one of the following supported Database Management Systems: c("oracle","postgresql","redshift","sql server","pdw", "netezza","bigquery","sqlite")
+#' @param user username for OMOP v5.0 instance
+#' @param password password for OMOP v5.0 instance
+#' @param server OMOP v5.0 server
+#' @param port NULL by default.
+#' @param schema NULL by default.
 #' @import DatabaseConnector
 #' @import dplyr
 #' @import ggplot2
 #' @import survminer
 #' @import survival
-#' @importFrom crayon red
 #' @export
 #'
 #'
@@ -47,19 +46,18 @@ plot_survival <- function(dbms = c("oracle","postgresql","redshift","sql server"
                                                        type = "right"))
 
         if (is.vector(survival_object)) {
-                cat(crayon::red("\n\tError: survival_time and/or event_occurred not in correct format. Please check and try again.\n"))
+                cat("\n\tError: survival_time and/or event_occurred not in correct format. Please check and try again.\n")
         } else {
                 km_fit_01 <- try_catch_error_as_na(survival::survfit(survival_object ~ cohort_cols,
                                                            data = dataframe))
                 if ((length(km_fit_01) == 1) & any(is.na(km_fit_01))) {
-                        cat(crayon::red("\n\tError: cohort_object and/or dataframe not in correct format. Please check and try again.\n"))
+                        cat("\n\tError: cohort_object and/or dataframe not in correct format. Please check and try again.\n")
 
                 } else {
                         medsurv <- survminer::surv_median(km_fit_01)
 
                         OUTPUT <- survminer::ggsurvplot(km_fit_01,
                                              data = dataframe,
-                                      #       pval = pval,
                                              xscale = 12,
                                              break.x.by = 6,
                                              legend = c(0.8, 0.9),
